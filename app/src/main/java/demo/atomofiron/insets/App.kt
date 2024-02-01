1e302dcf5fd6f9f39e522a1fd3f5d3ee3b72580d
atomofiron <atomofiron@gmail.com>
2024-02-01 07:45:04 +0100

    add dynamic colors to app theme

---
+++ app/src/main/java/demo/atomofiron/insets/App.kt
@@ -0,0 +1,12 @@
+package demo.atomofiron.insets
+
+import android.app.Application
+import com.google.android.material.color.DynamicColors
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
